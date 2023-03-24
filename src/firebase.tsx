c11513b43746f15a0b9eec822fe04fc67978d8b8
ruubyme <wngml1741@naver.com>
2023-03-24 09:47:13 +0900

    Feat : chatting-login
    login 기능 구현

--- src/firebase.tsx
+++ src/firebase.tsx
@@ -1,7 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "firebase/firestore/lite";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -22,4 +21,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-export const db = getFirestore(app);
+export { app, analytics };
